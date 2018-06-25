9a126021191ba552f3ff199596515e46d2cfc608
ssebugenyi <39189454+ssebugenyi@users.noreply.github.com>
2018-06-26 00:02:09 +0300

    first commit

---
+++ designs.js
@@ -0,0 +1,38 @@
+// Select color input
+// Select size input
+var height, size, color;
+
+
+$('#sizePicker').submit( function(event){
+	event.preventDefault();
+	height = $('#inputHeight').val();
+	width = $('#inputWeight').val();
+	makeGrid(height , width);
+})
+// When size is submitted by the user, call makeGrid()
+
+
+function makeGrid(x,y) {
+	$('tr').remove();
+
+for (var i = 1; i <= x; i++) {
+	$('#pixelCanvas').append('<tr id = table' + i + '></tr>');
+
+for (var j = 1; j <= y; j++) {
+	$('#table' + i ).append('<td></td>');
+	}}
+
+	$('td').click(function addColor(){
+		color = $('#colorPicker').val()
+	
+
+	if($(this).attr('style')){
+		$(this).removeAttr('style')
+	}
+	else{
+		$(this).attr('style', 'background-color:' + color);
+	}
+	})
+}
+
+
